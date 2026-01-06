d1c43a35aa90f847b8e8796f91563ad0144d5c52
sai goud <saikumarmogulla40@gmail.com>
2026-01-06 20:19:10 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -193,7 +193,7 @@ def call (Map configMap){
             
             stage('Deploy') {
                 when{
-                    expression { deploy_to == "dev" || deploy_to = "qa" || deploy_to = "qa" }
+                    expression { deploy_to == "dev" || deploy_to == "qa" || deploy_to == "qa" }
                 }
                 steps {
                     script{
